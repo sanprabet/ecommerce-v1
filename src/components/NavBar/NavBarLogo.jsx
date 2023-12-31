d9659866a70560b0f7956c90c94f6ae7796a9222
Santiago Prada <sanprabet@gmail.com>
2023-12-31 14:15:40 -0500

    Changed website name and img files

--- src/components/NavBar/NavBarLogo.jsx
+++ src/components/NavBar/NavBarLogo.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from "../../static/punto.png"
+import logo from "../../static/LosPuntosLogo.png"
 import { Link } from 'react-router-dom'
 import "./styles/NavBarLogo.scss"
 
